b0c64bf12b947c00157e68588169500a2d43edab
Jason H <jason@noreply>
2014-08-13 06:10:28 -0400

    PEP 8

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, Response
 import os, captcha
+
 app = Flask(__name__)
+
+
 @app.route("/Captcha")
 def viewcaptcha():
-    c=captcha.getCaptcha()
-    return Response(c[0],mimetype="image/jpeg")
+    c = captcha.getCaptcha()
+    return Response(c[0], mimetype="image/jpeg")
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0", debug=True)
